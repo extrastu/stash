789a4cad7187582204a575fa3d051e04488ca5f8
extrastu <1067633131@qq.com>
2022-07-09 18:05:30 +0800

    Update netease.js

--- netease.js
+++ netease.js
@@ -1,6 +1,6 @@
 $httpClient.get("https://tenapi.cn/comment/", function (error, response, data) {
 	const { song, content, name, sing } = data.data;
-	console.log(JSON.stringify(data));
+	console.log(song,content,name,sing);
 	$done({
 		title: "网易云热评",
 		content:
