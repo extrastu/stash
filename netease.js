ae55bf840b19a1ec9cf85f742fa69460b498293b
extrastu <1067633131@qq.com>
2022-07-09 17:56:45 +0800

    🧪 test: log

--- netease.js
+++ netease.js
@@ -1,5 +1,6 @@
 $httpClient.get("https://tenapi.cn/comment/", function (error, response, data) {
 	const { song, content, name, sing } = data;
+	console.log(JSON.stringify(data));
 	$done({
 		title: "网易云热评",
 		content:
